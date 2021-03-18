289889cf9f875757a661f9bd7b514b4a14d38ca9
veerleprins <veerleprins@hotmail.com>
2021-03-18 23:47:55 +0100

    Created renders & read json file

---
+++ src/routes/renders/home.js
@@ -0,0 +1,20 @@
+// Internal
+let fs = require("fs");
+
+const home = async (req, res, next) => {
+  try {
+    // Reading the file
+    const file = fs.readFileSync("./static/data/file.json");
+    const users = await Array.from(JSON.parse(file));
+
+    // Rendering the page:
+    res.render("index.ejs", {
+      pageTitle: "Home",
+      shirts: users[0].shirts,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = home;
