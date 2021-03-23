bd10823f05ae2eeea4280d6e0cf0c03a9f02fb57
veerleprins <veerleprins@hotmail.com>
2021-03-24 00:32:33 +0100

    Error trying to get button working. Not solved

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,20 @@
 // Externals
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Functions
 const home = require("./renders/home");
+const create = require("./renders/create");
 const design = require("./renders/design");
 const error = require("./renders/error");
 
 // Routes
-router.get("/", home).get("/design", design).get("/*", error);
+router
+  .get("/", home)
+  .get("/design/:id", design)
+  .post("/design/id", urlencodedParser, create)
+  .get("/*", error);
 
 module.exports = router;
