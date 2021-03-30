f5556e78ad6019b86d456348aaa3819d9bfd954f
veerleprins <veerleprins@hotmail.com>
2021-03-30 21:56:32 +0200

    Added all the new routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,11 +5,26 @@ const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Functions
-const home = require("./renders/home");
+const shop = require("./renders/shop");
+const designs = require("./renders/designs");
 const design = require("./renders/design");
+const addItem = require("./renders/addItem");
+const saveItem = require("./renders/saveItem");
+const cart = require("./renders/cart");
 const error = require("./renders/error");
+const thanks = require("./renders/thanks");
+const removeItem = require("./renders/removeItem");
 
 // Routes
-router.get("/", home).get("/design/:id", design).get("/*", error);
+router
+  .get("/", shop)
+  .get("/designs", designs)
+  .get("/designs/:id", design)
+  .get("/cart", cart)
+  .post("/cart", urlencodedParser, addItem)
+  .post("/", urlencodedParser, removeItem)
+  .post("/designs", urlencodedParser, saveItem)
+  .get("/thanks", thanks)
+  .get("/*", error);
 
 module.exports = router;
